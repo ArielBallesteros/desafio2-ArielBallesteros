e73aa985db5cae2af027859ee2ec6810341ddb6e
MSI <arielballesteros94@gmail.com>
2023-04-03 19:36:19 -0300

    tird commit

--- desafio2/index.js
+++ desafio2/index.js
@@ -1,6 +1,6 @@
 import ProductManager from "./manager/ProductManager.js";
 
-const manager = new ProductManager ();
+const manager = new ProductManager ('./files/Productos.json');
 
 const env = async () => {
     let prod ={
@@ -8,23 +8,31 @@ const env = async () => {
         description:"descripcion",
         price:2000,
         thumbnail:"imagen 4",
-        code:"code",
+        code:"6666666666",
         stock:10,
     }
 
  
 
-    let result = await manager.AddProducts(prod);
-    console.log(result)
+    // let result = await manager.addProducts(prod);
+    // console.log(result)
 
     let products = await manager.getProducts()
     console.log(products)
 
-    let productID = await manager.getProducById(5);
-    console.log(productID)
+    // let productID = await manager.getProducById(4);
+    // console.log(productID)
 
-    let borrarProdPorID = await manager.deleteProducById(9);
-    console.log(borrarProdPorID)
+    // let borrarProdPorID = await manager.deleteProductById(4);
+    // console.log(borrarProdPorID)
+
+    // let borrarTodo = await manager.deleteAllProducts();
+    // console.log(borrarTodo)
+
+    
+
+    let updateProduct = await manager.updateProduct("tinta tattoo","tinta negra",500,"foto","0000025",23,4);
+    console.log(updateProduct)
 }
 
 env()
